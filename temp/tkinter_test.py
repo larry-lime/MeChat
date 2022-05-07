7d7c5d6b33e5a34a878785641a0f29cde929e8d4
Lawrence Lim <ll4715@nyu.edu>
2022-05-08 02:11:45 +0800

    Finished testing tkinter GUI

--- temp/tkinter_test.py
+++ temp/tkinter_test.py
@@ -1,6 +1,72 @@
 #!/usr/bin/python
 
-import tkinter as tk
-top = tk.Tk()
-# Code to add widgets will go here...
-top.mainloop()
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
+
+class GUI:
+    def __init__(self):
+        self.gui_done = False
+        self.nickname = simpledialog.askstring("Nickname", "Enter a nickname:")
+
+    def gui_loop(self):
+        self.running = True
+        self.win = tkinter.Tk()
+        self.win.configure(bg="lightgray")
+
+        self.chat_label = tkinter.Label(self.win, text="Chat", bg="lightgray")
+        self.chat_label.configure(font=("Arial", 12))
+        self.chat_label.pack(padx=20, pady=5)
+
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state='disabled')
+
+        self.msg_label = tkinter.Label(self.win, text="Message", bg="lightgray")
+        self.msg_label.configure(font=("Arial", 12))
+        self.msg_label.pack(padx=20, pady=5)
+
+        self.input_area = tkinter.Text(self.win, height=3, width=30)
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = tkinter.Button(self.win, text="Send", command=self.write)
+        self.send_button.config(font=("Arial", 12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.gui_done = True
+        self.win.protocol("WM_DELETE_WINDOW", self.stop)
+        self.win.mainloop()
+
+    def write(self):
+        message = f"{self.nickname}: {self.input_area.get('1.0', tkinter.END)}"
+        print(message)
+        # self.sock.send(message.encode('utf-8'))
+        self.input_area.delete('1.0', 'end')
+
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        exit(0)
+
+    def recieve(self):
+        while self.running:
+            try: 
+                # message = self.sock.recv(1024)
+                message = "test"
+                if message == "NICK":
+                    # self.sock.send(self.nickname.encode('utf-8'))
+                    pass
+                else:
+                    if self.gui_done:
+                        self.text_area.config(state='normal')
+                        # self.text_area.insert(tkinter.END, message)
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state='disabled')
+            except:
+                break
+
+if __name__ == "__main__":
+    gui = GUI()
+    gui.gui_loop()
+    print("GUI closed")
